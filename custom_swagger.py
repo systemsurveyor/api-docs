6b4ac6c4ed3f20dbcf7b256c567920af2fe20e58
Brian Anderson <brian@systemsurveyor.com>
2023-01-23 13:46:07 -0600

    lint; remove sample, and add docs with example

--- custom_swagger.py
+++ custom_swagger.py
@@ -1,16 +1,19 @@
-custom_texts = {
-  '<h1 id="system-surveyor-api-folders">Folders</h1>': '<h3>Test</h3>',
-}
+from typing import Dict
+
+# the key is what is currently in the swagger document
+# the value is what will be appended after the value
+# example: '<h1 id="system-surveyor-api-folders">Folders</h1>': "<h3>Test</h3>"
+custom_texts: Dict[str, str] = {}
 
 # Read in the file
 file_path = "source/index.html.md"
-with open(file_path, 'r') as file :
-  filedata = file.read()
+with open(file_path, "r") as file:
+    filedata = file.read()
 
 # Replace the target string
 for key, value in custom_texts.items():
-  filedata = filedata.replace(key, key+value)
+    filedata = filedata.replace(key, key + value)
 
 # Write the file out again
-with open(file_path, 'w') as file:
-  file.write(filedata)
+with open(file_path, "w") as file:
+    file.write(filedata)
